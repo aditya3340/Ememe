fb7d65242797df498e3c5a88d3c8f922a376ad4e
aditya3340 <kumar.aditya3340@gmail.com>
2023-03-03 13:54:53 +0530

    initalize

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import hero from "../images/fg.png";
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <h2 className="header">EMEM MEME</h2>
+      </header>
+
+      <div className="hero">
+        <h1 className="hero-text">
+          Your <span>FACE</span> with favourite
+          <i>
+            <span> MEMES</span>
+          </i>
+        </h1>
+        <img src={hero} alt="cheems" />
+      </div>
+
+      <div className="hero-btn">
+        <button >TRY NOW</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
